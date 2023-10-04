382344acd0796721fe016aa9a21283ae8fc3d3af
ringcasey <97819928+ringcasey@users.noreply.github.com>
2023-10-04 15:41:00 +0000

    Restructured tasks.json

--- api/populate.sh
+++ api/populate.sh
@@ -2,6 +2,6 @@
 export $(grep -v '^#' .env | xargs)
 
 docker cp ./database/SchoolDirectoryCSV.csv ${DB_CONTAINER}:/schools.csv
-cat ./database/hspc_queries-psql.sql | docker exec -i ${DB_CONTAINER} psql -U $DB_USER -d $DB_NAME 
+cat ./api/database/hspc_queries-psql.sql | docker exec -i ${DB_CONTAINER} psql -U $DB_USER -d $DB_NAME 
 echo "copy School(SchoolName, AddressLine1, AddressLine2, City, \"State\", PostalCode, USDCode) FROM '/schools.csv' DELIMITER ',' CSV HEADER;" | docker exec -i ${DB_CONTAINER} psql -U $DB_USER -d $DB_NAME
-cat ./database/hspc_testpopulate.sql | docker exec -i ${DB_CONTAINER} psql -U $DB_USER -d $DB_NAME
+cat ./api/database/hspc_testpopulate.sql | docker exec -i ${DB_CONTAINER} psql -U $DB_USER -d $DB_NAME
