7bf17ce4b47538843b5b9cbe7647983a45c91958
TrentPow <73408275+TrentPow@users.noreply.github.com>
2024-02-17 23:27:57 +0000

    Added Constants File to Client Side

---
+++ client/src/_utilities/constants.js
@@ -0,0 +1,22 @@
+/*
+List of Constants to be used throughout the project
+*/
+
+/*
+Role Constants
+*/
+const VOLUNTEER = 20;
+const JUDGE = 40;
+const ADVISOR = 60;
+const ADMIN = 80;
+const MASTER = 100;
+const legalLevels = [VOLUNTEER, JUDGE, ADVISOR, ADMIN, MASTER];
+
+module.exports = {
+    VOLUNTEER,
+    JUDGE,
+    ADVISOR,
+    ADMIN,
+    MASTER,
+    legalLevels
+}
