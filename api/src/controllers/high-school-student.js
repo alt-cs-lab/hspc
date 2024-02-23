36ff78699c9fbc4ce4cf11cfca45c2fa16558efb
TrentPow <73408275+TrentPow@users.noreply.github.com>
2024-02-23 18:28:04 +0000

    Updated Backend for add student.

---
+++ api/src/controllers/high-school-student.js
@@ -0,0 +1,80 @@
+/*
+MIT License
+Copyright (c) 2024 KSU-CS-Software-Engineering
+*/
+const router = require("express").Router();
+const { check } = require("express-validator");
+const validator = require("validator");
+const isEmpty = require("is-empty");
+const passport = require("passport");
+const {
+  badRequestCheck,
+  useService,
+  minimumAccessLevelCheck,
+} = require("../utils/extensions.js");
+const constants = require("../utils/constants.js");
+const userService = require("../services/user");
+
+/**
+ * @api {post} /api/high-school-student/createStudent Register a new student
+ * @apiName createStudent
+ * @apiGroup HighSchoolStudent
+ *
+ * @apiBody {String} firstName First name of the student.
+ * @apiBody {String} lastName Last name of the student.
+ * @apiBody {String} email Valid email of the student.
+ * @apiBody {Number} [schoolId] School ID of the student.
+ * @apiBody {Number} [gradDate] Graduation Date is the date in which the student is graduating.
+ *
+ * @apiSuccess (Success 201) {JSON} message says {email} successfully registered
+ * @apiError (Bad Request 400) {String} error Error message for invalid request body data.
+ * @apiError (Internal Server Error 500) {String} error Error message for internal server errors.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *   {
+ *       First name is required.
+ *   }
+ */
+router.post('/createStudent',
+//passport.authenticate("jwt", { session: false }),
+//minimumAccessLevelCheck(constants.ADVISOR),
+//[
+//  check("userId").exists().withMessage("User ID is required."),
+//  // check is number
+//  check("userId").isNumeric().withMessage("User ID must be a number."),
+//],
+    [
+    check('firstName')
+        .isLength({max: 100}).withMessage('First name must be less than 100 characters.')
+        .not().isEmpty().withMessage("First name is required.")
+        .trim()
+        .escape(),
+    check('lastName')
+        .isLength({max: 100}).withMessage('Last name must be less than 100 characters.')
+        .not().isEmpty().withMessage("Last name is required.")
+        .trim()
+        .escape(),
+    check('email')
+        .not().isEmpty().withMessage("Email is required.")
+        .normalizeEmail()
+        //TWP TODO: Ensure Email is not in database
+        .custom(async value => {
+            try{
+                return await userService.getLogin()
+                    ? Promise.reject()
+                    : Promise.resolve()
+            }
+            catch {
+                return Promise.reject()
+            }
+        }).withMessage("That email is already in use."),
+    check('schoolId')
+        .not().isEmpty().withMessage("School is required."),
+    check('gradDate')
+        .not().isEmpty().withMessage("Graduation Date is empty.")
+], badRequestCheck, (req, res) => {
+    useService(userService.register, req, res, 'created');
+});
+
+module.exports = router;
