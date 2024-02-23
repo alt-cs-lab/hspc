afa80736e5455543a58b4f3df30eeb187ea928c9
devangriffin <89404422+devangriffin@users.noreply.github.com>
2024-02-23 18:28:45 +0000

    Updated Page

---
+++ api/src/controllers/high-school-student.js
@@ -0,0 +1,6 @@
+/*
+MIT License
+Copyright (c) 2024 KSU-CS-Software-Engineering
+*/
+
+module.exports = router;
--- api/src/services/high-school-student.js
+++ api/src/services/high-school-student.js
@@ -1,14 +1,27 @@
+/*
+MIT License
+Copyright (c) 2024 KSU-CS-Software-Engineering
+*/
+
 require("dotenv").config();
 
 const db = require("../utils/hspc_db").db;
-const constants = require("../utils/constants");
-const { renameKeys } = require("../utils/extensions");
+// const constants = require("../utils/constants");
+// const { renameKeys } = require("../utils/extensions");
 const bcrypt = require("bcrypt");
 
 module.exports = {
     addHighSchoolStudent
 }
 
-function addHighSchoolStudent(firstName, lastName, school, email, gradDate) {
-    // FINISH
+function addHighSchoolStudent(firstName, lastName, schoolID, email, gradDate) {
+    return db.none(
+    `
+        INSERT INTO HighSchoolStudents (FirstName, LastName, SchoolID, Email, GradDate)
+        VALUES($(firstName), $(lastName), $(schoolID), $(email), $(gradDate))    
+    `,
+    {
+        firstName, lastName, schoolID, email, gradDate
+    }
+    );
 }
